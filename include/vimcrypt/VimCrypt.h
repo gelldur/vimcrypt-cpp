e5eecd29a53d1906fe70135cc641feeb4dbe40a4
Dawid Drozd <dawid.drozd@mobica.com>
2018-07-13 10:59:09 +0200

    Add parsing VimCrypt header

---
+++ include/vimcrypt/VimCrypt.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <array>
+#include <istream>
+#include <ostream>
+#include <vector>
+
+class VimCrypt
+{
+public:
+	VimCrypt(std::istream& data);
+
+	std::vector<char> decode(const std::string& password);
+	std::vector<char> encode(const std::string& password);
+
+	enum class Encoded
+	{
+		none,
+		zip, // VimCrypt~01!
+		blowfish, // VimCrypt~02!
+		blowfish2, // VimCrypt~03!
+	};
+	static Encoded forName(const std::string& name);
+
+	struct Header
+	{
+		/**
+		 *  File header
+		 *  +------------+--------+--------+
+		 *  |   Magic    |  Salt  |   IV   |
+		 *  +------------+--------+--------+
+		 *  |VimCrypt~02!|01234567|01234567|
+		 *  +------------+--------+--------+
+		 **/
+		std::array<char, 12> _magic;
+		std::array<char, 8> _salt;
+		std::array<char, 8> _IV;
+
+		Encoded _encode = Encoded::none;
+
+		Header();
+
+		/**
+		 * @return header size in bytes
+		 * */
+		constexpr std::size_t size() const noexcept;
+	};
+	static Header readHeader(std::istream& data);
+
+	friend std::ostream& operator<<(std::ostream& stream, const VimCrypt& vimCrypt);
+	friend std::ostream& operator<<(std::ostream& stream, const VimCrypt::Encoded& encoded);
+
+private:
+	Header _header;
+	std::vector<char> _data;
+};
---
+++ src/VimCrypt.cpp
@@ -0,0 +1,129 @@
+#include "vimcrypt/VimCrypt.h"
+
+#include <cassert>
+#include <iterator>
+#include <sstream>
+
+VimCrypt::VimCrypt(std::istream& data)
+{
+	std::noskipws(data); // prevent from skipping whitechars
+
+	std::copy(std::istream_iterator<char>(data),
+	          std::istream_iterator<char>(),
+	          std::back_inserter(_data));
+
+	try
+	{
+		std::string tmpData;
+		std::copy(_data.begin(), _data.end(), std::back_inserter(tmpData));
+		std::istringstream tmpStream{tmpData};
+		_header = readHeader(tmpStream);
+		// If everything went well we can erase header from data
+		_data.erase(_data.begin(), _data.begin() + _header.size());
+	}
+	catch(const std::invalid_argument& ex)
+	{
+		// ignore, probably plaintext not encoded
+	}
+}
+
+VimCrypt::Header::Header()
+{
+	_magic.fill('\0');
+	_salt.fill('\0');
+	_IV.fill('\0');
+}
+
+constexpr std::size_t VimCrypt::Header::size() const noexcept
+{
+	return _magic.max_size() + _salt.max_size() + _IV.max_size();
+}
+
+VimCrypt::Header VimCrypt::readHeader(std::istream& data)
+{
+	Header header;
+	auto& magic = header._magic;
+	auto& salt = header._salt;
+	auto& IV = header._IV;
+
+	if(data.readsome(magic.data(), magic.max_size()) != magic.max_size())
+	{
+		throw std::invalid_argument("Invalid header - can't read magic");
+	}
+
+	header._encode = forName(std::string{magic.begin(), magic.end()});
+	if(header._encode == Encoded::none)
+	{
+		throw std::invalid_argument("Invalid header - none");
+	}
+
+	if(data.readsome(salt.data(), salt.max_size()) != salt.max_size())
+	{
+		throw std::invalid_argument("Invalid header - can't read salt");
+	}
+
+	if(data.readsome(IV.data(), IV.max_size()) != IV.max_size())
+	{
+		throw std::invalid_argument("Invalid header - can't read IV vector");
+	}
+	return header;
+}
+
+std::vector<char> VimCrypt::decode(const std::string& password)
+{
+	return {};
+}
+
+std::vector<char> VimCrypt::encode(const std::string& password)
+{
+	throw std::runtime_error("Not implemented");
+}
+
+VimCrypt::Encoded VimCrypt::forName(const std::string& name)
+{
+	if(name == "VimCrypt~01!")
+	{
+		return Encoded::zip;
+	}
+	if(name == "VimCrypt~02!")
+	{
+		return Encoded::blowfish;
+	}
+	if(name == "VimCrypt~03!")
+	{
+		return Encoded::blowfish2;
+	}
+	return Encoded::none;
+}
+
+std::ostream& operator<<(std::ostream& stream, const VimCrypt& vimCrypt)
+{
+	stream << "Magic:" << vimCrypt._header._magic.data()
+	       << ": salt:" << vimCrypt._header._salt.data() << ": IV:" << vimCrypt._header._IV.data()
+	       << ":\nEncoded with: " << vimCrypt._header._encode << "\nData:";
+
+	std::copy(vimCrypt._data.begin(), vimCrypt._data.end(), std::ostream_iterator<char>(stream));
+	return stream;
+}
+
+std::ostream& operator<<(std::ostream& stream, const VimCrypt::Encoded& encode)
+{
+	switch(encode)
+	{
+	case VimCrypt::Encoded::none:
+		stream << "none";
+		break;
+	case VimCrypt::Encoded::zip:
+		stream << "zip";
+		break;
+	case VimCrypt::Encoded::blowfish:
+		stream << "blowfish";
+		break;
+	case VimCrypt::Encoded::blowfish2:
+		stream << "blowfish2";
+		break;
+	default:
+		stream << "todo implement encode name";
+	}
+	return stream;
+}
